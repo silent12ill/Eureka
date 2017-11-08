fe5492ee59a75eaf2666d2a0d3b3ef50f8e523f7
Vasanth Kesavan <vk432@njit.edu>
2017-11-08 15:14:21 -0500

    added authentication and express sessions

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dbUri = require('./dbInfo').dbUri;
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 /* Connection to the database */
 
@@ -28,6 +29,43 @@ const UserSchema = new Schema({
         require: true
     }
 });
+
+/* authenticate input against database */
+
+UserSchema.statics.authenticate = function (email, password, callback) {
+    User.findOne({ email: email })
+        .exec(function (err, user) {
+            if (err) {
+                return callback(err)
+            } else if (!user) {
+                var err = new Error('User not found.');
+                err.status = 401;
+                return callback(err);
+            }
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (result === true) {
+                    return callback(null, user);
+                } else {
+                    return callback();
+                }
+            })
+        });
+}
+
+
+/* hashing a password before saving it to the database */
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    bcrypt.hash(user.password, 10, function (err, hash){
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
+
 
 /* Model for Schema */
 
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,18 @@ const app = express();
 
 
 app.listen(process.env.PORT || 3000);
+
+/* use sessions for tracking login */
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false
+}));
+
+/* parse incoming requests */
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static('../client/src/index.html'));
 
 
@@ -15,3 +26,21 @@ app.use(express.static('../client/src/index.html'));
 
 app.post('/api/signup', userSignUp);
 app.post('/api/signin', userSignIn);
+
+/* catch 404 and forward to error handler */
+
+app.use(function (req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/*
+* define as the last app.use callback
+* error handler
+*/
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
