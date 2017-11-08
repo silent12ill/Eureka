b71e6dbf475a3f065f0e3e76d0972ea7529db23a
Isabella Tea <tea.isabella@gmail.com>
2017-11-07 20:36:39 -0800

    implemented react-slick carousels on index and dashboard

--- client/src/components/TopVideos.js
+++ client/src/components/TopVideos.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
 import { Affix, Row, Col, Icon } from 'antd';
-
 import video from '../images/videoThumbnail.jpg';
+import Slider from 'react-slick';
 
-const TopVideos = function() {
-  return (
-    <div className='topVideosContainer'>
-      <h1 className='title'><a name='explore'>Popular Videos</a></h1>
-	      <div className='topVideosContainerInner'>
-		      <Icon type="caret-left" className='arrowTop' />
-		      <img src={video} className='exampleVideo' />
-					<img src={video} className='exampleVideo' />
-					<img src={video} className='exampleVideo' />
-					<img src={video} className='exampleVideo' />
-					<img src={video} className='exampleVideo' />
-					<img src={video} className='exampleVideo' />
-					<img src={video} className='exampleVideo' />
-					
-					<Icon type="caret-right" className='arrowTop' />
-				</div>
-    </div>
-  )
+
+export default class TopVideos extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 400,
+      slidesToShow: 6,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+        <div className='topVideosContainer'>
+        <h1 className='title'><a name='explore'>Popular Videos</a></h1>
+            <Slider {...settings}>
+              <div><img src={video} className='exampleVideo' /></div>
+              <div><img src={video} className='exampleVideo' /></div>
+              <div><img src={video} className='exampleVideo' /></div>
+              <div><img src={video} className='exampleVideo' /></div>
+              <div><img src={video} className='exampleVideo' /></div>
+              <div><img src={video} className='exampleVideo' /></div>
+              <div><img src={video} className='exampleVideo' /></div>
+            </Slider>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default TopVideos;
+// const TopVideos = function() {
+//   return (
+//     <div className='topVideosContainer'>
+//       <h1 className='title'><a name='explore'>Popular Videos</a></h1>
+// 	      <div className='topVideosContainerInner'>
+// 		      <Icon type="caret-left" className='arrowTop' />
+// 		      <img src={video} className='exampleVideo' />
+// 					<img src={video} className='exampleVideo' />
+// 					<img src={video} className='exampleVideo' />
+// 					<img src={video} className='exampleVideo' />
+// 					<img src={video} className='exampleVideo' />
+// 					<img src={video} className='exampleVideo' />
+// 					<img src={video} className='exampleVideo' />
+					
+// 					<Icon type="caret-right" className='arrowTop' />
+// 				</div>
+//     </div>
+//   )
+// }
+
+// export default TopVideos;
 
 
     //   <Row type="flex" justify="space-around">
--- client/src/components/TopicDashboard.js
+++ client/src/components/TopicDashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../css/style.css';
 import { Layout, Menu, Steps, Icon } from 'antd';
@@ -8,31 +8,87 @@ import TopVideos from './TopVideos';
 import { Radio } from 'antd';
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
+import Slider from 'react-slick';
 
-class TopicDashboard extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-      //state here
-		};
-	}
+// class TopicDashboard extends React.Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+//       //state here
+// 		};
+// 	}
 
-  //functions here
+//   //functions here
 
+//   render() {
+//      const settings = {
+//       dots: true,
+//       infinite: true,
+//       speed: 500,
+//       slidesToShow: 1,
+//       slidesToScroll: 1
+//     };
+
+
+// 		return (
+// 		  <div>
+//       <div className='navbg'>
+//         <TopicNav />
+//       </div>
+//         <TopicHeader />
+
+
+//         <div className='videoInfoBox'>
+//           <div className='videoTitleContainer'>
+//             <span className='videoTitle'>Recognize the Signs and Symptoms of Stroke </span><br />
+//             <span className='videoDesc'>Description | Date Posted | Some Other Info</span><br />
+//           </div>
+//           <div className='votingContainer'>
+//             <RadioGroup defaultValue="a" size="large">
+//               <RadioButton value="d"><Icon type="share-alt" style={{ fontSize: 20 }} /></RadioButton>
+//               <RadioButton value="a"><Icon type="frown" style={{ fontSize: 20 }} /></RadioButton>
+//               <RadioButton value="b"><Icon type="bulb" style={{ fontSize: 40 }} /></RadioButton>
+//               <RadioButton value="c"><Icon type="smile" style={{ fontSize: 20 }} /></RadioButton>
+//               <RadioButton value="d"><Icon type="heart" style={{ fontSize: 20 }} /></RadioButton>
+//             </RadioGroup>
+//           </div>
+//         </div>
+
+//         <div className="vidRecContainer">
+//           <TopVideos />
+//         </div>
+//       </div>
+// 		)
+//   }
+// }
+
+// export default TopicDashboard;
+
+
+
+export default class TopicDashboard extends Component {
   render() {
-		return (
-		  <div>
-      <div className='navbg'>
-        <TopicNav />
-      </div>
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 400,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+
+        <div className='navbg'>
+          <TopicNav />
+        </div>
         <TopicHeader />
 
-        <div className='videoContainer'>
-          <div className='videoContainerInner'>
-          <Icon type="caret-left" className='arrowLeft' />
-            <iframe width="760" height="515" src="https://www.youtube.com/embed/mkpbbWZvYmw" frameborder="0" allowfullscreen></iframe>
-          <Icon type="caret-right" className='arrowRight' />
-          </div>
+        <div className='topVideosContainer'>
+          <Slider {...settings}>
+            <div className='videoDisplay'><iframe width="760" height="515" src="https://www.youtube.com/embed/mkpbbWZvYmw" frameborder="0" allowfullscreen></iframe></div>
+            <div className='videoDisplay'><iframe width="760" height="515" src="https://www.youtube.com/embed/mkpbbWZvYmw" frameborder="0" allowfullscreen></iframe></div>
+            <div className='videoDisplay'><iframe width="760" height="515" src="https://www.youtube.com/embed/mkpbbWZvYmw" frameborder="0" allowfullscreen></iframe></div>
+          </Slider>
         </div>
 
         <div className='videoInfoBox'>
@@ -54,12 +110,13 @@ class TopicDashboard extends React.Component {
         <div className="vidRecContainer">
           <TopVideos />
         </div>
+
+
       </div>
-		)
+    );
   }
 }
 
-export default TopicDashboard;
 
     // <Header className="header">
     //   <div className="logo" />
