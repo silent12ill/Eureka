00697f2c66a69dfd6bb97ac339a3cee3e9e935d5
Isabella Tea <tea.isabella@gmail.com>
2017-11-11 14:42:43 -0800

    basic get/post requests

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,8 +17,9 @@ class App extends React.Component {
       currentPage: 'home',
       loggedIn: true,
       currentUser: 'guest',
+      userBookmarks: [],
       playlist: [], //playlist of videos; each video an object of -- needs thumbnails, urls, titles, descriptions, etc.
-
+      
       //for guest
       currentTopic: "",
 
@@ -69,17 +70,116 @@ class App extends React.Component {
 * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 // post - send authentication info
+  authenticate() {
+  	axios.post('/authenticate', {
+  		params: {
+  			username: username,
+  			password: password
+  		}
+  	})
+  	.then((response) => {
+  		console.log(response);
+  		//on success
+  		  //getPlaylist()
+  		  //getBookmarks()
+  	})
+  }
 
 // get - playlist for category for guest user
+  getPlaylistByTopic(topic) {
+  	axios.get('/getPlaylistByTopic', {
+      params: {
+        topic: topic,
+      }
+    })
+    .then((response) => {
+      //add retrieved playlist to state
+      var videos = response.data.items;
+      this.setState({playlist: videos})
+    })
+  }
 // get - users playlist based on preferences, upvotes and downvotes
+  getPlaylistByUser() {
+  	axios.get('/getPlaylistByUser', {
+      params: {
+        username: this.state.currentUser
+      }
+    })
+    .then((response) => {
+      //add retrieved playlist to state
+      var videos = response.data.items;
+      this.setState({playlist: videos})
+    })
+  }
 
 // post - preferences for specific user
+  postUserTopics(topics) {
+  	axios.post('postUserTopics', {
+  		params: {
+  			username: this.state.currentUser,
+  			topics: topics
+  		}
+  	})
+  	.then((response) => {
+  		//if success, alert success
+  		//getPlaylistByUser(); //retrieves playlist after topics/prefs sent
+  	})
+  }
+
 // post - upvote and downvotes for specific video
+  postVote(video, vote) {
+  	axios.post('/postUserVote', {
+  		params: {
+  			currentUser: this.state.currentUser,
+  			url: video,
+  			vote: vote
+  		}
+  	})
+  	.then((response) => {
+  		//on success, alert success
+  		//getPlaylistByUser() ? right away?
+  	})
+  }
 
 // post - users bookmarked videos
+  postUserBookmark(url) {
+  	axios.post('/postUserBookmark', {
+  		params: {
+  			currentUser: this.state.curentUser,
+  			url: url
+  		}
+  	})
+  	.then((response) => {
+      //on success, alert success
+      getUserBookmarks() //update bookmark list
+  	})
+  }
+
 // get - users bookmarked videos
+  getUserBookmarks() {
+    axios.get('/getUserBookmarks', {
+    	params: {
+    		currentUser: this.state.currentUser
+    	}
+    })
+  }
+
 
 // post - user submitted video
+  addVideo(url) {
+  	axios.post('/submittedVideo', {
+  		params: {
+  			source: source,
+  			url: '',
+  			currentUser: ''
+  		}
+  	})
+  	.then((response) => {
+  		//on success, alert success!
+  	})
+  
+  }
+
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * *
