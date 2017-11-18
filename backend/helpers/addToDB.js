e4d6884f29eab01916403c0e20d75f5f8b586436
Abishak Kodi <abishak.kodi@gmail.com>
2017-11-18 11:30:19 -0800

    Added getToDB

---
+++ backend/helpers/addToDB.js
@@ -0,0 +1,27 @@
+const Video;
+const _ = require('underscore');
+
+
+module.exports = addToDB = (res, req) => {
+ /* Check the request body and assign it to an object */
+
+    // if(req.body.email && req.body.password) {
+    //     let userData = {
+    //         email: req.body.email,
+    //         password: req.body.password
+    //     };
+
+    //     /* Use the above object to save it to the database */
+    //     User.create(userData, (err) => {
+    //         if(err) {
+    //             console.error(err);
+    //         };
+    //         console.log('Saved Successfully');
+    //     });
+    // } else {
+    //   // res.body = 'you fucked up and did not have all the data'
+    // }
+
+
+
+}
