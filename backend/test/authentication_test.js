a50deca3aed3047aca54196eab26e8605323f4c2
Vasanth Kesavan <vk432@njit.edu>
2017-12-03 12:39:45 -0500

    signup and signin in a single testfile, also added test cases

--- backend/test/authentication_test.js
+++ backend/test/authentication_test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const  User = require('../db').User;
+const User = require('../db').User;
 const bcrypt = require('bcrypt');
 
 describe('testing the signup, password hashing', () => {
@@ -23,6 +23,24 @@ describe('testing the signup, password hashing', () => {
               assert(userObj[0].password !== 'test1234');
               done();
           });
+   });
+
+   it('fails for wrong credentials', (done) => {
+       User.findOne({ email: 'test-user@test.com'})
+           .then((userObj) => {
+                bcrypt.compare('test123', userObj.password, (err, response) => {
+                   assert(response === false);
+                });
+           }).then(done, done);
+   });
+
+   it('passes for the correct credentials', (done) => {
+       User.findOne({ email: 'test-user@test.com'})
+           .then((userObj) => {
+               bcrypt.compare('test1234', userObj.password, (err, response) => {
+                   assert(response === true);
+               });
+           }).then(done, done);
    });
 });
 
