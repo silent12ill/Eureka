f2f8a8707da0ebb1e715f1c930ac3f716ad82dba
Isabella Tea <tea.isabella@gmail.com>
2017-12-10 22:07:27 -0800

    code cleanup for video players

--- client/src/components/Dashboard/PlayerDailyMotion.js
+++ client/src/components/Dashboard/PlayerDailyMotion.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 
 const PlayerDailyMotion = function(props) {
   return (
-    <iframe frameborder="0" width="780" height="570" src={`//www.dailymotion.com/embed/video/${props.videoID}`} allowFullScreen></iframe>
+    <iframe frameBorder="0" width="780" height="570" src={`//www.dailymotion.com/embed/video/${props.videoID}`} allowFullScreen></iframe>
   )
 }
 
--- client/src/components/Dashboard/RecentVideo.js
+++ client/src/components/Dashboard/RecentVideo.js
@@ -4,7 +4,7 @@ import video from '../../images/videoThumbnail.jpg';
 
 const RecentVideo = function(props) {
   return (
-    <div className='recentVideo' onClick={props.playClickedVideo}>
+    <div className='recentVideo' onClick={() => props.playClickedVideo(props.video)}>
       <img src={video} />
       <span className='recentVideoTitle'>{props.title}</span> <br />
       <span className='recentVideoCategory'>{props.category}</span>
