9daf77548515b742f4dd2a5349512c43d13207d6
Abishak Kodi <abishak.kodi@gmail.com>
2017-12-11 22:52:01 -0800

    Started to implement redux and react router

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,43 @@ import App from './components/App';
 import { LocaleProvider } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 
+//createStore makes the main store
+//combine reducers makes all the reducers into one object
+//apply middleware to store redux data
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+//Provider is what
+import { Provider } from 'react-redux';
+
+//stores the history?
+import createHistory from 'history/createBrowserHistory';
+
+//Route is pretty self explanatory
+import { Route } from 'react-router';
+
+//special redux and router components needed
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const reducers = combineReducers({
+    // ...reducers,
+    router: routerReducer
+  });
+
+const store = createStore(
+reducers,
+  composeWithDevTools(applyMiddleware(middleware)));
+
+
+
+
+
+
 
 // const PrimaryLayout = () => (
 
@@ -30,4 +67,12 @@ import enUS from 'antd/lib/locale-provider/en_US';
 
 // )
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+
+  , document.getElementById('app'))
