5a7259aea72850fa500d91b1eb593c6883054efb
Vasanth Kesavan <vk432@njit.edu>
2017-12-13 08:20:33 -0500

    conflict fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App';
