faaebf58b9e5af919d9b525e5e3e2fb566cdb5f1
Abishak Kodi <abishak.kodi@gmail.com>
2017-12-16 10:37:21 -0800

    Added Jest testing

--- client/__tests__/App.test.js
+++ client/__tests__/App.test.js
@@ -1,11 +1,11 @@
 import React from "react";
-//import App from '../src/components/App.js';
+import App from '../src/components/App';
 
 describe('Jest Set Up Syntax', ()=> {
   it('knows 2 + 2 equals 4', ()=>{
     expect(2 + 2).toBe(4);
-
   })
 });
+
 
 
