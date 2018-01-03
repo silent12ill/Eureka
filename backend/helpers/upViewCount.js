4782e3f5f73e57cce8e112f4901dc86676282447
Vasanth Kesavan <vk432@njit.edu>
2018-01-03 12:49:55 -0500

    video view count incremented successfully

--- backend/helpers/upViewCount.js
+++ backend/helpers/upViewCount.js
@@ -1,5 +1,22 @@
-const upViewCount = () => {
+/*
+    1. Receive videoId
+    2. Find the video, increment the count.
+*/
 
+const Video = require('../db').Video;
+
+const upViewCount = (req, res) => {
+    let videoId = req.body.params.videoId;
+
+    Video.findOne({videoId: videoId}, (err, data) => {
+        if(err) {
+            throw err;
+        } else {
+            data.viewCount.$inc();
+            data.save();
+            res.status(200).send("View Count incremented successfully");
+        }
+    });
 };
 
 module.exports = upViewCount;
