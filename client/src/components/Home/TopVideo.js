c1639b16dd07f06eaf6c4e47d7c018ff349674bc
Isabella Tea <tea.isabella@gmail.com>
2018-01-05 20:51:03 -0800

    getTopVideos rendering to homepage

--- client/src/components/Home/TopVideo.js
+++ client/src/components/Home/TopVideo.js
@@ -10,11 +10,12 @@ const TopVideo = function(props) {
 
   return (
     <div className="topvideo" onClick={ playClickedVideo }>
-      <div className='home-img-wrapper'>
-        <img src={ props.video.thumbnail } />
+      <div className='home-wrapper hvr-sweep-to-top'>
+        <img src={ props.video.thumbnail } /> <br />
         <div className='home-img-overlay'>
           <img src='https://i.pinimg.com/originals/45/5d/d4/455dd42e78bd3a8ff66b88eb65c815f1.png' />
         </div>
+          <p className='home-title-overlay'> {props.video.title} </p>
       </div>
     </div>
   )
