2a721eccf1409be38281646fb87cf08debfafe64
Isabella Tea <tea.isabella@gmail.com>
2018-01-07 19:50:56 -0800

    remove unused addToDB and underscore

--- backend/helpers/addVideo.js
+++ backend/helpers/addVideo.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const Video = require('../db').Video;
 const Queue = require('../db').Queue;
-const addToDB = require('./addToDB');
 const ytKey = "AIzaSyDZxJwtWfdNWheLpKk-lrLbXLHaiWZfp2s";
 const vimKey = "53dea8ea88fb59d8aab79d5083257416";
 const dmKey = "";
